c9222e86ab34d3afa0031983fc7d0085091c4871
collins <kipkorirc583@gmail.com>
2023-06-21 13:19:32 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  let response;
+  
+  if (ride <= 400) {
+    response = 'This one is on me!';
+  } else if (ride > 2000 && ride <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (ride > 2500) {
+    response = 'No can do.';
+  }
+  
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips) {
+  switch(tips){
+   case "generous":
+   return "Thank you so much.";
+   case "not as generous":
+   return "Thank you.";
+   default: 
+   return "Bye.";
+}
 }
